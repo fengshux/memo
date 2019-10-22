6b577d24fd8f0f9d8d3886ac7790a766fcdff08e
xuxiaoyu <xuxy@debian>
2019-10-22 13:35:49 +0800

    expiretion

--- memo.go
+++ memo.go
@@ -12,18 +12,26 @@ type Memo struct {
 	rw sync.RWMutex
 }
 
-
 func New( expire time.Duration ) *Memo {
-
-	return &Memo{expire, map[string]interface{}{}, map[string]time.Time{}, sync.RWMutex{}}
+	m := &Memo{expire, map[string]interface{}{}, map[string]time.Time{}, sync.RWMutex{}}
+	ticker := time.NewTicker(1 * time.Minute)
+	done := make(chan bool)
+	go func () {
+		for {
+			select {
+			case <-done:
+				return
+			case <-ticker.C:
+				m.purge()
+			}
+		}
+	}()
+	return m
 }
-
 
 func Default() *Memo {
 	return New(5 * time.Minute)
 }
-
-
 
 func (m *Memo) Get(key string) interface{} {
 	
@@ -41,14 +49,41 @@ func (m *Memo) Get(key string) interface{} {
 	return val
 }
 
-func (m *Memo) Set(key string, val interface{})  {
-	
+func (m *Memo) Set(key string, val interface{})  {	
 	current := time.Now()	
 	m.rw.Lock()
 	last, ok := m.time[key]
-	if ok && current.After(last) {
+	if !ok {
+		m.cache[key] = val
+		m.time[key] = current		
+	} else if current.After(last) {
 		m.cache[key] = val
 		m.time[key] = current
-	}		
+	}
 	m.rw.Unlock()
+}
+
+func (m *Memo) purge() {
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
+	
+	keys := []string{}
+	m.rw.RLock()
+	for k , _ := range m.cache {
+		keys = append(keys, k)
+	}
+	m.rw.RUnlock()
+	for _, k := range keys {
+		m.rw.RLock()
+		createTime := m.time[k]
+		m.rw.RUnlock()
+		if createTime.Add(m.expire).Before(time.Now()) {
+			m.rw.Lock()
+			delete(m.cache, k)
+			delete(m.time, k)
+			m.rw.Unlock()			
+		}
+	}
 }
--- memo_test.go
+++ memo_test.go
@@ -9,9 +9,7 @@ import (
 
 func TestSetAndGet(t *testing.T) {
 
-
 	memo := Default()
-
 	for i := 0 ; i < 1000; i++ {
 		go func (){
 			memo.Set("key", i)
